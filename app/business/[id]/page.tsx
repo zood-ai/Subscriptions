16d152e9e5a2ef770c146f29e5789f2bb86c02d7
AbdelrahmanZood <20007@zood.ai>
2025-12-18 12:03:59 +0200

    feat: Introduce initial application pages for home, dashboard, authentication, and business sections.

---
+++ app/business/[id]/page.tsx
@@ -0,0 +1,13 @@
+interface BusinessPageProps {
+  params: Promise<{ id: string }>
+}
+
+
+export default async function Business({ params }: BusinessPageProps) {
+  const { id } = await params;
+  return (
+    <div>
+      <h1>Business  - {id}</h1>
+    </div>
+  )
+}
