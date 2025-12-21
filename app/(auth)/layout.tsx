b40d220a641637b61e5ab07dce280d1ae274a9a8
MoemenAdam <mmuummnna@gmail.com>
2025-12-21 13:48:56 +0200

    Replace logo image from PNG to SVG format and update layout structure to use a fragment instead of a main tag.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import Logo from '@/assets/logo.png';
+import Logo from '@/assets/logo.svg';
 
 const layout = ({
   children,
@@ -8,14 +8,14 @@ const layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <main>
+    <>
       <nav className="p-4">
         <Image src={Logo} alt="App Logo" width={50} height={62} />
       </nav>
       <section className="p-4 sm:p-[30px] mx-auto w-full sm:w-[550px] ">
         {children}
       </section>
-    </main>
+    </>
   );
 };
 
