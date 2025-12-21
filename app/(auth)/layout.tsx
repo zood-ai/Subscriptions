c985a5a4a04e44ec53847ef5e2ed04b04773772c
MoemenAdam <mmuummnna@gmail.com>
2025-12-21 14:37:34 +0200

    refactor: Update layout structure by replacing section with main tag for semantic clarity and remove unused test file

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,9 +12,9 @@ const layout = ({
       <nav className="p-4">
         <Image src={Logo} alt="App Logo" width={50} height={62} />
       </nav>
-      <section className="p-4 sm:p-[30px] mx-auto w-full sm:w-[550px] ">
+      <main className="p-4 sm:p-[30px] mx-auto w-full sm:w-[550px] ">
         {children}
-      </section>
+      </main>
     </>
   );
 };
--- lib/actions/test.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-const Test = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  return await res.json();
-};
-
-export { Test };
