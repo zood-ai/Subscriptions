d0971241215b2c9aadc976f8301064aa10266deb
abdelrahman-mohammed1 <UGS.141356@ci.suez.edu.eg>
2025-12-24 11:11:46 +0200

    chore: remove console.log from proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function proxy(request: NextRequest) {
-  console.log(request.nextUrl);
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
