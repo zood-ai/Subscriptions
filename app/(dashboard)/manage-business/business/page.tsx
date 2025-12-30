cf0c0165246642ffa21509a02738d58872fdbd64
abdelrahman-mohammed1 <UGS.141356@ci.suez.edu.eg>
2025-12-30 14:27:54 +0200

    refactor: Improve code structure and formatting in Businesses component

--- app/(dashboard)/manage-business/business/page.tsx
+++ app/(dashboard)/manage-business/business/page.tsx
@@ -1,15 +1,51 @@
-import Table from './components/Table';
-import TableHeader from '@/components/TableHeader';
-import Query from '@/lib/Query';
-import CreateForm from './components/CreateForm';
+import Table from "./components/Table";
+import TableHeader from "@/components/TableHeader";
+import Query from "@/lib/Query";
+import CreateForm from "./components/CreateForm";
 
+export interface BusinessTypesResponce {
+  status: boolean;
+  code: number;
+  message: string;
+  data: BusinessType[];
+}
+
+export interface BusinessType {
+  id: string;
+  name: string;
+}
+
+export interface CountriesResponce {
+  status: boolean;
+  code: number;
+  message: string;
+  data: Country[];
+}
+
+export interface Country {
+  id: string;
+  name_en: string;
+}
 
 export default async function Businesses() {
-  const { data: { data: countries } } = await Query({ api: 'v1/manage/countries' });
-  const { data: { data: businessTypes } } = await Query({ api: 'v1/manage/business-types' })
+  const countries = await Query<CountriesResponce>({
+    api: "v1/manage/countries",
+  });
+  const businessTypes = await Query<BusinessTypesResponce>({
+    api: "v1/manage/business-types",
+  });
   return (
     <>
-      <TableHeader CreateForm={<CreateForm countries={countries} businessTypes={businessTypes} />} title="Business" createBtnTitle="Add Business" />
+      <TableHeader
+        CreateForm={
+          <CreateForm
+            countries={countries.data?.data || []}
+            businessTypes={businessTypes?.data?.data || []}
+          />
+        }
+        title="Business"
+        createBtnTitle="Add Business"
+      />
       <Table />
     </>
   );
