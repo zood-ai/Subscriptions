f1525b0c4451b7f1efbb72edbca2f0ba6ffe5efb
abdelrahman-mohammed1 <UGS.141356@ci.suez.edu.eg>
2026-01-12 15:41:42 +0200

    feat: Add popover component and integrate it into NavBar for improved user interaction

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,35 +1,68 @@
-'use client';
-import { User, Menu } from 'lucide-react';
-import { useState } from 'react';
-import Sidebar from './Sidbar';
+"use client";
+
+import { User, Menu, LogOut } from "lucide-react";
+import { useState } from "react";
+import Sidebar from "./Sidbar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "../ui/button";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function NavBar() {
+  const router = useRouter();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
+  const handleLogout = () => {
+    Cookies.remove("token");
+    router.push("/login");
   };
 
   return (
     <>
-      <header className="fixed left-0 md:left-64 right-0 top-0 z-30 flex h-16 items-center border-b border-border bg-background px-6 transition-all duration-300 ease-in-out">
+      <header className="fixed left-0 md:left-64 right-8 -top-2.5 z-30 flex h-16 items-center border-b border-border bg-background px-6">
         <div className="flex w-full items-center justify-between">
-          <div className="flex items-center gap-4 md:hidden">
-            <button onClick={toggleMobileMenu} className="cursor-pointer">
+          <div className="md:hidden">
+            <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
               <Menu className="h-5 w-5 text-muted-foreground" />
             </button>
           </div>
-          <div className="w-full flex justify-end">
-            <User className="text-muted-foreground bg-[#FAFAFA] cursor-pointer" />
+
+          <div className="flex justify-end w-full">
+            <Popover>
+              <PopoverTrigger asChild>
+                <button className="rounded-full p-2 hover:bg-muted cursor-pointer transition">
+                  <User className="h-5 w-5 text-muted-foreground" />
+                </button>
+              </PopoverTrigger>
+
+              <PopoverContent
+                align="end"
+                className="w-44 p-2 rounded-xl border bg-background shadow-md"
+              >
+                <Button
+                  variant="danger"
+                  onClick={handleLogout}
+                  className="w-full cursor-pointer bg-white justify-start gap-2 text-sm text-red-600 hover:bg-red-50 hover:text-red-600"
+                >
+                  <LogOut className="h-4 w-4" />
+                  Logout
+                </Button>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </header>
+
       {isMobileMenuOpen && (
         <div
           className="fixed inset-0 z-50 bg-black/50 md:hidden"
           onClick={() => setIsMobileMenuOpen(false)}
         />
       )}
+
       <Sidebar
         isMobileView
         isMobileMenuOpen={isMobileMenuOpen}
