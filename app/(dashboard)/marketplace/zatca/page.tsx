d032b4485aee1feaa51d827f5d2f4fd63203b59c
abdelrahman-mohammed1 <UGS.141356@ci.suez.edu.eg>
2026-01-18 12:56:43 +0200

    fix: remove unused showReference filter from CustomTable component

--- app/(dashboard)/marketplace/zatca/page.tsx
+++ app/(dashboard)/marketplace/zatca/page.tsx
@@ -34,7 +34,6 @@ export default function Businesses() {
           columns={columns}
           filters={{
             showName: true,
-            showReference: true,
           }}
           actions={actions}
           onClickRow={(data) => {}}
