133b3dbfb5892c80135440c1324c2690ce490433
MoemenAdam <mmuummnna@gmail.com>
2026-02-01 10:55:19 +0200

    feat: Consolidate login form functionality into the login page component, removing the separate Form component for improved structure and maintainability

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,92 @@
-import Form from './Form';
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import useCustomMutation from '@/lib/Mutation';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+import { useForm, useWatch } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  email: z.email('Invalid email'),
+  password: z.string().min(6, 'Password is required and at least 6 characters'),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
+interface LoginResponse {
+  token: string;
+}
 
 export default function Login() {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const formValues = useWatch({ control });
+
+  const { mutate, isPending, error } = useCustomMutation<
+    FormData,
+    LoginResponse
+  >({
+    api: 'v1/super-admin/login',
+    method: 'POST',
+    options: {
+      onSuccess: (data) => {
+        Cookies.set('token', data.token);
+        router.push('/dashboard');
+      },
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    mutate(data);
+  };
+
   return (
     <>
       <h1 className="text-[25px] font-semibold pb-[25px]">Sign in</h1>
-      <Form />
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6.25">
+        <Input
+          Label="Email"
+          animateLabel
+          type="email"
+          error={errors?.email?.message}
+          value={formValues.email}
+          {...register('email')}
+        />
+
+        <Input
+          Label="Password"
+          type="password"
+          animateLabel
+          value={formValues.password}
+          error={errors?.password?.message}
+          {...register('password')}
+        />
+
+        {error && (
+          <div className="text-red-500 text-sm">{error.data?.message}</div>
+        )}
+
+        <div className="flex justify-end pt-5">
+          <Button loading={isPending} type="submit" variant="primary">
+            {isPending ? 'Loading...' : 'Login'}
+          </Button>
+        </div>
+      </form>
     </>
   );
 }
